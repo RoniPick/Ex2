11080ea574569801dfca569166314b2a34763aca
RoniPick <92802335+RoniPick@users.noreply.github.com>
2021-11-24 16:45:56 +0200

    update problems

--- my_mat.c
+++ my_mat.c
@@ -4,6 +4,7 @@
 int mat[10][10];
 int min(int, int);
 
+//this function entering the numbers into a matrix
 void entermat(int mat[10][10]) {
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
