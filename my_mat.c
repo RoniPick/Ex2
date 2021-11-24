db4ada86f46bd19920667fce178e3b3ea80c2af3
RoniPick <92802335+RoniPick@users.noreply.github.com>
2021-11-24 14:58:56 +0200

    update problems

--- my_mat.c
+++ my_mat.c
@@ -5,16 +5,16 @@
 void entermat(int mat[10][10]) {
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            scanf( "%d", mat[i][j]);
+            scanf( "%d", &mat[i][j]);
         }
     }
     floyd_warshall_algo(mat);
 }
 
 void floyd_warshall_algo(int mat[10][10]){
-    for(int k=1; k<=10; k++){
-        for(int i=1; i<=10; i++){
-            for(int j=1; j<=10; j++){
+    for(int k=0; k<10; k++){
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
                 mat[i][j] = min(mat[i][j], mat[i][k] + mat[k][j]);
             }
         }
